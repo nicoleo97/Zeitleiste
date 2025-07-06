a2b67923f38904f1b1c6244e973880a9e829902e
nicoleo97 <73941175+nicoleo97@users.noreply.github.com>
2025-07-06 14:34:19 +0000

    draft1 gestartet

--- draft01.py
+++ draft01.py
@@ -1,7 +1,32 @@
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv(data/test.csv)
+data = pd.read_csv('data/test.csv')
 
-st.title(Zeitleiste)
-st.write(data)
+
+
+auswahl_dict = {-4000:'4000 v. Chr.',
+                -3000:'3000 v. Chr.',
+                -2000:'2000 v. Chr,',
+                -1000:'1000 v. Chr.',
+                -500:'500 v. Chr.',
+                0:'Zeitwende',
+                500:'500 n. Chr.',
+                1000:'1000 n. Chr.',
+                1500:'16. Jhdt.',
+                1600:'17. Jhdt.',
+                1700:'18. Jhdt.',
+                1800:'19. Jhdt.',
+                1900:'20. Jhdt.',
+                2000:'21. Jhdt.'}
+
+reverse_auswahl_dict = {v: k for k, v in auswahl_dict.items()}
+
+auswahl_texte = list(auswahl_dict.values())
+auswahl_zahl = list(auswahl_dict.keys())
+
+auswahl = st.select_slider('Wähle einen Zeitraum',options=auswahl_texte, value='20. Jhdt.')
+
+st.header(auswahl)
+st.subheader(f"{reverse_auswahl_dict[auswahl]+1} bis {reverse_auswahl_dict[auswahl]+100}")
+
