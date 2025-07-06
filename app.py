761766a9bc904e8778bdc88506e228471c00c5a7
nicoleo97 <73941175+nicoleo97@users.noreply.github.com>
2025-07-06 10:45:07 +0000

    commit

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import numpy as np
 st.title('Zeitleiste')
 
 
-# Century
+###########################################################
+# JAHRHUNDERT AUSWAHL
 century = list(range(-4000,2200,100))
 
 century_slider = st.select_slider('Jahrhundert',options=century,value=2000)
@@ -13,17 +14,28 @@ century_slider = st.select_slider('Jahrhundert',options=century,value=2000)
 if 0 < century_slider <= 1500:
     act_label = str(abs(century_slider))
     act_cent = st.container()
-    act_cent.title(f"{act_label} n. Chr.")
+    act_cent.header(f"{act_label} n. Chr.")
 
 elif century_slider == 0:
-    act_cent.title(Zeitwende)
+    act_cent.title("Zeitwende")
 
 elif century_slider < 0:
     act_label = str(abs(century_slider))
     act_cent = st.container()
-    act_cent.title(f"{act_label} v. Chr.")
+    act_cent.header(f"{act_label} v. Chr.")
 
 elif century_slider > 1500:
     act_label = str(abs(century_slider))
     act_cent = st.container()
-    act_cent.title(f"{act_label}er Jahre")
+    act_cent.header(f"{act_label}er Jahre")
+
+###########################################################
+
+data = pd.read_csv('data/test.csv', sep=',')
+
+start = century_slider 
+ende = century_slider + 100
+filter = list(range(start,ende))
+
+data_filtered = data[data['jahr'].isin(filter)]
+st.write(data_filtered)
