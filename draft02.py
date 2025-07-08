d27f0f5ad906545bc0f7b6a2faa8940208b3a806
nicoleo97 <73941175+nicoleo97@users.noreply.github.com>
2025-07-08 17:24:44 +0000

    draft02 final shaping

--- draft02.py
+++ draft02.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+#with st.sidebar:
+ #   kee = st.text_input('Password?')
+  #  if kee != '7B':
+   #     st.error('Falsches Passwort!')
+    #    st.stop()
+
+
+
+
 st.title("Zeitleiste")
 
 tab16, tab17, tab18, tab19, tab20, tab21, input_tab = st.tabs(['16.Jhdt.', '17.Jhdt.', '18.Jhdt.', '19.Jhdt.', '20.Jhdt.', '21.Jhdt.','Input'])
@@ -41,6 +50,14 @@ def Badge_Fach(fach):
         st.badge('MATHE',color='blue')
     elif fach == 'Physik':
         st.badge('PHYSIK', color='orange')
+    elif fach == 'Deutsch':
+        st.badge('DEUTSCH', color='red')
+    elif fach == 'Latein':
+        st.badge('LATEIN', color='red')
+    elif fach == 'Geschichte':
+        st.badge('GESCHICHTE', color='green')
+    elif fach == 'BE' or 'Musik':
+        st.badge('KUNST & MUSIK', color='violet')
     else:
         st.badge(fach, color='gray')
 
@@ -68,14 +85,25 @@ def Inputs(events_df):
 
 # Verwaltung von Tabs
 with input_tab:
-    auswahl_fach = optionen
+    auswahl_fach = ['Mathe', 'Physik', 'Deutsch','Latein', 'Geschichte', 'BE', 'Musik', 'Other']
     input_form = st.form('Neuer Eintrag')
     input_form.header('Neuen Eintrag hinzufügen')
     input_jahr = input_form.number_input('Jahr', min_value=1501, max_value=2100, step=1, value=1997,help='Erreignisse vor der Zeitwende mit Minus eingeben, z.B. -500 für 500 v.Chr.')
-    input_fach = input_form.selectbox('Fach', options=optionen, index=0, help='Zuordnung zu einem Unterrichtsfach')
+    input_fach = input_form.selectbox('Fach', options=auswahl_fach, help='Zuordnung zu einem Unterrichtsfach',index=2)
     input_ereignis = input_form.text_input('Ereignis', help='Kurze Bezeichnung des Ereignisses, z.B. "Erfindung des Buchdrucks"')
     input_infotext = input_form.text_area('Infotext', help='Längere Beschreibung des Ereignisses, z.B. "Johannes Gutenberg erfindet den Buchdruck mit beweglichen Lettern"')
-    input_form.form_submit_button('Absenden')
+    sender = input_form.form_submit_button('Vorschlag abschicken')
+
+    if sender:
+        # Neuen Eintrag in die CSV-Datei schreiben
+        new_event = pd.DataFrame({
+            'Jahr': [input_jahr],
+            'Fach': [input_fach],
+            'Ereignis': [input_ereignis],
+            'Infotext': [input_infotext]
+        })
+        new_event.to_csv('data/inputs.csv', mode='a', header=False, index=False)
+        st.success('Neuer Eintrag erfolgreich abgeschickt!')
 
 
 with tab21:
